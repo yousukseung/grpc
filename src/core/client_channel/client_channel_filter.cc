fce9ce6958b2ad7d6eb182faef393df10f951f4a
Yousuk Seung <ysseung@google.com>
2024-08-08 16:08:29 -0700

    client_channel_filter:669 nosteal

--- src/core/client_channel/client_channel_filter.cc
+++ src/core/client_channel/client_channel_filter.cc
@@ -666,7 +666,7 @@ class ClientChannelFilter::SubchannelWrapper final
             ApplyUpdateInControlPlaneWorkSerializer(state, status);
             Unref();
           },
-          DEBUG_LOCATION);
+          DEBUG_LOCATION, /*nosteal=*/true);
     }
 
     grpc_pollset_set* interested_parties() override {
